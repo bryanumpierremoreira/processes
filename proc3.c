444e6d303628fdd0345b56b890fd762ad98d28d4
Bryan Umpierre Moreira <bryanumpierremoreira@google.com>
2023-04-17 08:44:08 -0300

    function get_ten() was adjusted to get the hundred and ten of the number receiving the collatz_value pointer. The function name has been changed to get_subset().

--- proc3.c
+++ proc3.c
@@ -7,7 +7,16 @@
 
 int collatz(int x)
 {
-    
+    /*
+    Decrease the number until you reach 1. If the number is even,
+    divide it by 2 otherwise multiply it by 3 and add 1
+
+    e.g.:
+
+    x = 35
+
+    will print: 35, 106, 53, 160, 80, 40, 20, 10, 5, 16, 8, 4, 2, 1 
+     */
     printf("x = %d: %d, ", x, x);
     
     while (x != 1) {
@@ -25,8 +34,19 @@ int collatz(int x)
     return 0;
 }
 
-int dez(int me, int *valor_collatz)
+int get_subset(int me, int *valor_collatz)
 {
+    /*
+    This function takes the process pid in the form of a string and
+    returns the position number: string[-2] and string[-1].
+
+    e.g: 
+    
+    int s = 12345
+    get_dezena(s) --> returns 34
+    */
+    
+    printf("\nVou pegar a centena e dezena do id do processo filho\n");
     char texto[256];
     char dezena[256];
 
@@ -37,11 +57,12 @@ int dez(int me, int *valor_collatz)
     dezena[1] = texto[size_me-1];
     printf("\ncentena funcao: %s\n", dezena);
     int valor;
-    int *ptr = &valor;
+    //int *ptr = &valor;
     valor = atoi(dezena);
     printf("\nvalor inteiro: %d\n\n",valor);
 
-    valor_collatz = ptr;
+    *valor_collatz = valor;
+    printf("\ncentena + dezena: %s\n", dezena);
     return 0;
 }
 
@@ -51,7 +72,10 @@ int main()
     int status;
     int n = 0;
     int valor_collatz = 0;
+    int val_collatz_teste = 1;
     char texto[256];
+
+    
 
     printf("Digite o range de processos filhos: ");
     scanf("%d", &n);
@@ -66,6 +90,7 @@ int main()
     int size_me = sizeof(me);
     parent = getppid();
     
+
     // esse print sera mostrado N vezes pq o filho comeca no trecho acima
     printf("\nMeu pid: %d, \tpid pai: %d, \tpid novo: %d\n\n", me, parent, newPid);
 
@@ -84,20 +109,13 @@ int main()
     }
     else
     {
-        printf("Sou o filho\n");
-        printf("Vou pegar a centena e dezena do id do processo filho\n");
-        char dezena[256];
+        printf("\nSou o filho\n");
+        
+        get_subset(me, &valor_collatz);
 
-        dezena[0] = texto[size_me-2];
-        dezena[1] = texto[size_me-1];
-        printf("\ncentena: %s\n", dezena);
-        int valor;
-
-        valor = atoi(dezena);
-        //printf("\n%d\n\n\n",valor);
-        if (valor !=0)
+        if (valor_collatz !=0)
         {
-            collatz(valor);
+            collatz(valor_collatz);
         }
         
         
